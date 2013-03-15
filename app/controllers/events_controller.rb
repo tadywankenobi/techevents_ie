bbffb9bd3e72a20f524c404a458309ab5a45d37f
Brian Kenny <brian@bkenny.com>
2013-03-15 11:16:41 +0000

    Group events by months and only display future events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,8 @@ require 'events_import/pub_standards'
 
 class EventsController < ApplicationController	
 	def index
-		@events = Event.all
+		@events = Event.where("starting_at > ?", Time.now)
+		@event_months = @events.group_by { |t| t.starting_at.beginning_of_month }
 	end
 
 	def show
