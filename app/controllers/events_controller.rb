0a10b55d2f5a28721e35ad26133a77d9acf4c41b
Brian Kenny <brian@bkenny.com>
2013-03-26 13:38:26 +0000

    Fix ordering of events by starting_at

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController	
 	def index
-		@events = Event.where("starting_at > ?", Time.now)
+		@events = Event.where("starting_at > ?", Time.now).order('starting_at asc')
 		@event_months = @events.group_by { |t| t.starting_at.beginning_of_month }
 	end
 
