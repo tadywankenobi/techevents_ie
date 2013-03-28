e0b7ae22dce5f6b17ccb148de9b33f1db84b7b84
Brian Kenny <brian@bkenny.com>
2013-03-28 12:42:41 +0000

    Display foursquare photos of a venue

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
-class EventsController < ApplicationController	
+require 'events_import/events_import'
+
+class EventsController < ApplicationController
 	def index
 		@events = Event.where("starting_at > ?", Time.now).order('starting_at asc')
 		@event_months = @events.group_by { |t| t.starting_at.beginning_of_month }
@@ -6,5 +8,10 @@ class EventsController < ApplicationController
 
 	def show
 		@event = Event.find(params[:id])
+
+		if @event.venue.fs_id?
+			venue = EventsImport::Venues.new
+			@photos = venue.fetch_photos @event.venue
+		end
 	end
 end
--- lib/events_import/events_import.rb
+++ lib/events_import/events_import.rb
@@ -6,13 +6,19 @@ require 'events_import/providers/lanyrd'
 
 module EventsImport
 	class Venues
+
+		@@client = Foursquare2::Client.new(:client_id => ENV['FS_ID'], :client_secret => ENV['FS_SECRET'])
+
 		def fetch_fs_id venue
-			client = Foursquare2::Client.new(:client_id => ENV['FS_ID'], :client_secret => ENV['FS_SECRET'])
-			fs_venue = Hashie::Mash.new(client.search_venues(:ll => "#{venue.lat},#{venue.lng}", :query => venue.title)).groups.first.items.first
+			fs_venue = Hashie::Mash.new(@@client.search_venues(:ll => "#{venue.lat},#{venue.lng}", :query => venue.title)).groups.first.items.first
 			if fs_venue
 				venue.fs_id = fs_venue.id
 				venue.save
 			end
+		end
+
+		def fetch_photos venue
+			Hashie::Mash.new(@@client.venue_photos(venue.fs_id, options = {:group => 'venue'})).items
 		end
 	end
 end
