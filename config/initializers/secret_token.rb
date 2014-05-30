f70233e51f9376396bf0d446c78a7a10cdce078b
Brian Kenny <brian@bkenny.com>
2014-05-30 09:34:41 +0100

    Rails secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TecheventsIe::Application.config.secret_token = ENV['RAILS_SECRET']
+TecheventsIe::Application.config.secret_token = "1ed47abdfd9ff192141c3b5d62600f1b30fc460cf529e3db7281f3bd4b88be08680a11422b59a8af4ceae2c531357d6cd24fadce9d0bd5a6d96c8d01112a63fc"
